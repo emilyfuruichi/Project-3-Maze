ef73bed763cc7fe93dba9f2fbc163659b317407d
Emily <60939799+emilyfuruichi@users.noreply.github.com>
2020-04-23 20:24:12 -0700

    Comment Cell
    no change to the code

--- maze/Cell.java
+++ maze/Cell.java
@@ -1,3 +1,7 @@
+/**
+ * Cell contains the coordinates and cell number for each cell of a maze
+ */
+
 package maze;
 
 public class Cell 
@@ -6,8 +10,10 @@ public class Cell
 	private int y; //coordinate y increasing down
 	private int cellNum; //individual cell ID
 	
-	/*
+	/**
 	 * for creating interior cells of the maze
+	 * @param num is cell number
+	 * @param size is the row/col length
 	 */
 	public Cell(int num, int size)
 	{
@@ -16,8 +22,11 @@ public class Cell
 		y = num/size;
 	}
 	
-	/*
+	/**
 	 * for creating the entry and exit points of the maze
+	 * @param num is cell number
+	 * @param x coordinate
+	 * @param y coordinate
 	 */
 	public Cell(int num, int x, int y)
 	{
@@ -26,21 +35,37 @@ public class Cell
 		this.y = y;
 	}
 	
+	/**
+	 * get the x coordinate
+	 * @return x coordinate value
+	 */
 	public int getX()
 	{
 		return x;
 	}
 	
+	/**
+	 * get the y coordinate
+	 * @return y coordinate value
+	 */
 	public int getY()
 	{
 		return y;
 	}
 	
+	/**
+	 * get the cell number
+	 * @return cell number
+	 */
 	public int getCellNum()
 	{
 		return cellNum;
 	}
 	
+	/**
+	 * creates a string of the coordinates and cell num
+	 * @return string of the cell values
+	 */
 	public String toString()
 	{
 		//return "[("+x+","+y+"):"+cellNum+"] ";
